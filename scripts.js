60930016b7d1e54f564e240a3f19ff21a2c2431d
Seif71 <sisoegypt9@gmail.com>
2025-10-21 14:08:46 -0400

    Updated

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-document.getElementById("changeButton").addEventListener("click", () => {
-    document.getElementById("mainText").innerText = "Thanks for visiting, Seif!";
+document.getElementById("showModel").addEventListener("click", function() {
+    const model = document.getElementById("modelContainer");
+    model.style.display = model.style.display === "none" ? "block" : "none";
   });
   
